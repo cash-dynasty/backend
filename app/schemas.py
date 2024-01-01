b1880b23c1b2fcc933e627ae5f4f81637ef5dba2
Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
2024-01-01 21:05:00 +0100

    CAS-78 Setup sqlalchemy and esablish connection with db. Add alembic (#1)
    * CAS-78 Setup sqlalchemy and esablish connection with db. Add alembic to handle migrations
    
    * refactor: small changes, mainly with importing files (CAS-78)
    
    ---------
    
    Co-authored-by: Marcin Wiatrak <marcin.wiatrak.95gmail.com>
    Co-authored-by: Amadeusz Rydlewski <amadeusz.rydlewski@bluesoft.com>

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,19 @@
 from typing import Union
+
 from pydantic import BaseModel
 
 
+# ////////// AUTH //////////
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+# ////////// USER //////////
 class User(BaseModel):
     username: str
     email: Union[str, None] = None
