4f62b42bd722ac082de15f6d3dd179c1732d0f15
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2024-01-04 23:24:27 +0100

    feat: add email sending handler with basic email html template

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,7 @@
+import resend
 from passlib.context import CryptContext
 
+resend.api_key = "re_gKSV3gL4_HERpk4eofjnbJ6Z36wH5dTyf"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
@@ -9,3 +11,21 @@ def verify_password(plain_password: str, hashed_password: str):
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
+
+
+def send_user_create_email_confirmation(email: str, token: str):
+    with open("templates/testemail.html", "r") as file:
+        html = file.read()
+        html = html.replace("{{{TOKEN}}}", token)
+    params = {
+        "from": "Developer CashDynasty <no-replay@cashdynasty.pl>",
+        "to": [email],
+        "subject": "Chyba działa :D",
+        "html": html,
+    }
+    email = resend.Emails.send(params)
+    return email
+
+
+# print(send_user_create_email_confirmation('dev@cashdynasty.pl', 'tokenik'))
+
