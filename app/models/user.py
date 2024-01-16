a913bf8adc446470d35c026a7b5e1f2914f15df4
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2024-01-16 23:30:50 +0100

    feat: create activation_tokens table

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 
 
 class User(Base):
@@ -9,3 +9,12 @@ class User(Base):
     password = Column(String)
     email = Column(String, unique=True, index=True)
     is_active = Column(Boolean, default=False)
+
+
+class ActivationToken(Base):
+    __tablename__ = "activation_tokens"
+
+    id = Column(Integer, primary_key=True)
+    token = Column(String)
+    expiration_date = Column(DateTime)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
